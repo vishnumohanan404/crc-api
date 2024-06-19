778ee2ae332a40593ca6c52d8439b6b63753bfe7
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-19 21:46:34 +0530

    vpc and subnet provi'd

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,47 @@
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr_block" {
+  type = string
+}
+
+variable "private_subnet_cidr_block" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
 # create a private subnet for dynamodb
+resource "aws_vpc" "vpc" {
+  cidr_block         = var.vpc_cidr
+  enable_dns_support = true
+
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_cidr_block
+  tags = {
+    Name = "public-Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnet_cidr_block
+  tags = {
+    Name = "private-Subnet"
+  }
+}
+
---
+++ infra/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region = "ap-south-1"
+vpc_cidr = "10.0.0.0/16"
+public_subnet_cidr_block  = "10.0.1.0/24"
+private_subnet_cidr_block  = "10.0.2.0/24"
+availability_zones = ["ap-south-1a", "ap-south-1b"]
